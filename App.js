7963aef533b77affdf4ce834a031d926e83e20e0
Francesco Scuccimarri <francescoscuccimarri@gmail.com>
2020-10-01 09:58:03 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// First component
 export default class App extends React.Component {
   render() {
     return (
